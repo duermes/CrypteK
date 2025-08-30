392d12e7b75ff8d8db3b446703ff16164ee64880
duermes <iloveposhitos@gmail.com>
2025-08-30 18:55:37 -0500

    las

---
+++ front/cryptek/src/lib/contract-router.ts
@@ -0,0 +1,42 @@
+import {createInstance, SepoliaConfig} from "@zama-fhe/relayer-sdk";
+
+
+const instance = await createInstance(SepoliaConfig);
+
+
+const {publicKey, privateKey} = await instance.generateKeypair();
+
+const eip712 = instance.createEIP712(
+  publicKey,
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+);
+
+const signature = async (userAddress: string) =>
+  await window.ethereum.request({
+    method: "eth_signTypedData_v4",
+    params: [userAddress, JSON.stringify(eip712)],
+  });
+
+const encryptedInput = async (value: string, userAddress: string) =>
+  await instance.createEncryptedInput(value, {
+    contractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    userAddress: userAddress,
+  });
+
+const decryptedInput = async (
+  encryptedHandle: string,
+  privateKey: string,
+  publicKey: string,
+  signature: string,
+  contractAddress: string,
+  userAddress: string
+) => {
+  return await instance.userDecrypt(
+    encryptedHandle,
+    privateKey,
+    publicKey,
+    signature,
+    contractAddress,
+    userAddress
+  );
+};
