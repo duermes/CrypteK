aab237d7d78a6c7987f6d3d47bd59f0ab66f7261
duermes <iloveposhitos@gmail.com>
2025-08-30 14:07:56 -0500

    no errors frontend

--- front/cryptek/src/app/test/layout.tsx
+++ front/cryptek/src/app/test/layout.tsx
@@ -1,30 +1,8 @@
 "use client";
-
-import {WagmiProvider} from "wagmi";
-import {createConfig, http} from "wagmi";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {liskSepolia, filecoinCalibration} from "wagmi/chains";
-
-const config = createConfig({
-  chains: [liskSepolia, filecoinCalibration],
-  transports: {
-    [liskSepolia.id]: http(),
-    [filecoinCalibration.id]: http(),
-  },
-});
-
-const queryClient = new QueryClient();
+import "@rainbow-me/rainbowkit/styles.css";
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body>
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </WagmiProvider>
-      </body>
-    </html>
+    <div>{children}</div>
   );
 }
